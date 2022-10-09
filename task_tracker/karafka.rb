af2781eae3b63d13ee5b6e3df30946e2ef899aa8
Roman Dubrovsky <r.dubrovsky@datarockets.com>
2022-10-09 08:45:32 +0200

    [Homework 2] Add consumers for karafka

---
+++ task_tracker/karafka.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class KarafkaApp < Karafka::App
+  setup do |config|
+    config.kafka = { 'bootstrap.servers': '127.0.0.1:9092' }
+    config.client_id = 'TaskTracker'
+    # Recreate consumers with each batch. This will allow Rails code reload to work in the
+    # development mode. Otherwise Karafka process would not be aware of code changes
+    config.consumer_persistence = !Rails.env.development?
+  end
+
+  # Comment out this part if you are not using instrumentation and/or you are not
+  # interested in logging events for certain environments. Since instrumentation
+  # notifications add extra boilerplate, if you want to achieve max performance,
+  # listen to only what you really need for given environment.
+  Karafka.monitor.subscribe(Karafka::Instrumentation::LoggerListener.new)
+  # Karafka.monitor.subscribe(Karafka::Instrumentation::ProctitleListener.new)
+
+  routes.draw do
+    topic :'accounts-stream' do
+      consumer AccountChanges
+    end
+
+    topic :'accounts' do
+      consumer AccountChanges
+    end
+  end
+end
